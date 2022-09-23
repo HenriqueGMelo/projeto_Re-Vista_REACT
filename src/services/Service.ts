a4f203a898fd7c15dd9b8c12a7c477a2031b6bf2
Henrique Melo <Henrique-125@hotmail.com>
2022-09-23 17:02:42 -0300

    Produtos

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://revista-ecommerce.herokuapp.com'
+    baseURL: 'http://localhost:5000'
 })
 
 export const login = async(url: any, dados: any, setToken: any, setDataUser: any) => {
